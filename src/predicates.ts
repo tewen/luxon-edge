520d8b92c9c41ab26ca10d568a660bb0ad8b1c57
Trevor Ewen <trevor@junctionconcepts.com>
2024-11-27 16:38:38 -0500

    Simple weekday predicates.

--- src/predicates.ts
+++ src/predicates.ts
@@ -1,4 +1,13 @@
-import type { DateTime } from 'luxon';
+import { DateTime } from 'luxon';
+
+export const isLeapYear = (dateTime: DateTime) => {
+  const beginningOfYear = dateTime.startOf('year');
+  const { days } = beginningOfYear
+    .plus({ year: 1 })
+    .diff(beginningOfYear, ['days'])
+    .toObject();
+  return days === 366;
+};
 
 export const isWithinMonth = (dateTime: DateTime, month: DateTime) => {
   return dateTime >= month.startOf('month') && dateTime <= month.endOf('month');
@@ -20,11 +29,30 @@ export const isWeekend = (dateTime: DateTime) => {
   return [6, 7].includes(dateTime.weekday);
 };
 
-export const isLeapYear = (dateTime: DateTime) => {
-  const beginningOfYear = dateTime.startOf('year');
-  const { days } = beginningOfYear
-    .plus({ year: 1 })
-    .diff(beginningOfYear, ['days'])
-    .toObject();
-  return days === 366;
+export const isMonday = (dateTime: DateTime = DateTime.now()) => {
+  return dateTime.weekday === 1;
+};
+
+export const isTuesday = (dateTime: DateTime = DateTime.now()) => {
+  return dateTime.weekday === 2;
+};
+
+export const isWednesday = (dateTime: DateTime = DateTime.now()) => {
+  return dateTime.weekday === 3;
+};
+
+export const isThursday = (dateTime: DateTime = DateTime.now()) => {
+  return dateTime.weekday === 4;
+};
+
+export const isFriday = (dateTime: DateTime = DateTime.now()) => {
+  return dateTime.weekday === 5;
+};
+
+export const isSaturday = (dateTime: DateTime = DateTime.now()) => {
+  return dateTime.weekday === 6;
+};
+
+export const isSunday = (dateTime: DateTime = DateTime.now()) => {
+  return dateTime.weekday === 7;
 };
